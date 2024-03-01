42cf35b8771d7898d99f71128ff270685778ac71
Griske24 <justas.griskevicius@gmail.com>
2024-03-01 23:24:25 +0200

    Create MorseTests.cs

---
+++ Ciphers.NET/Ciphers.NET.Tests.Unit/MorseTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ciphers.NET.Tests.Unit
+{
+    public class MorseTests
+    {
+        [Fact]
+        public void Encrypt_ShouldReturnEncryptedMessage()
+        {
+            // Arrange
+            string message = "Hello, World!";
+            string expectedEncryptedMessage = ".... . .-.. .-.. --- --..-- / .-- --- .-. .-.. -.. -.-.--";
+
+            // Act
+            string encryptedMessage = Morse.Encrypt(message);
+
+            // Assert
+            Assert.Equal(expectedEncryptedMessage, encryptedMessage);
+        }
+
+        [Fact]
+        public void Decrypt_ShouldReturnDecryptedMessage()
+        {
+            // Arrange
+            string message = ".... . .-.. .-.. --- --..-- / .-- --- .-. .-.. -.. -.-.--";
+            string expectedDecryptedMessage = "HELLO, WORLD!";
+
+            // Act
+            string decryptedMessage = Morse.Decrypt(message);
+
+            // Assert
+            Assert.Equal(expectedDecryptedMessage, decryptedMessage);
+        }
+    }
+}
